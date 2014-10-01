a068815834711f573ef1d75c36a6a4a95c98f0e1
Zach Fogelson <fogelsonz606@gmail.com>
2014-10-01 01:05:29 -0400

    boxplot added

--- homework/hw2/problem2.py
+++ homework/hw2/problem2.py
@@ -14,6 +14,7 @@ import numpy.linalg as lin # module for performing linear algebra operations
 import scipy
 import pprint
 import datetime as dt
+import math
 pp = pprint.PrettyPrinter(indent=4)
 
 novemberFirst = dt.datetime.strptime("2012-11-01", "%Y-%m-%d") 
@@ -44,20 +45,45 @@ for i in xrange(0,1000):
     samps.append(samp/float(med))
 npSamps = np.asarray(samps)
 
-plt.hist(map(lambda x: x*100,npSamps))
+# plt.hist(map(lambda x: x*100,npSamps))
 # plt.show()
 
-npSamps.std()
+one_trial_std = npSamps.std()
 
 means = []
+stds = []
 for i in xrange(0,1000):
     generation = []
     for j in xrange(0,inNovember.count()[0]):
         generation.append(np.random.binomial(med, .53)/float(med))
     means.append(np.mean(np.array(generation)))
+    stds.append(np.array(generation).std())
 
-plt.hist(map(lambda x: x*100,means))
-plt.show()
+# plt.hist(map(lambda x: x*100,means))
+# plt.show()
+nineteen_trial_std = np.array(means).std()
+
+# The ratio of the standard errors is reduced by a factor of the square root
+# of the number of samples per trial.
+
+stds_100 = map(lambda x: x*100, stds)
+
+# plt.hist(map(lambda x: x*100, stds))
+
+actual_std = inNovember["Obama"].std()
+
+pp.pprint(actual_std)
+pp.pprint(np.array(stds_100).mean())
+
+pp.pprint(actual_std/np.array(stds_100).mean())
+
+
+
+
+
+
+
+
 
 
 
---
+++ homework/hw2/problem3.py
@@ -0,0 +1,45 @@
+import os
+import copy
+import sys
+import subprocess
+import requests 
+from StringIO import StringIO
+import numpy as np
+import pandas as pd # pandas
+import matplotlib.pyplot as plt # module for plotting 
+import zipfile
+import StringIO
+import datetime as dt # module for manipulating dates and times
+import numpy.linalg as lin # module for performing linear algebra operations
+import scipy
+import pprint
+import datetime as dt
+import math
+import re
+import matplotlib.dates as mdates
+pp = pprint.PrettyPrinter(indent=4)
+
+startupinfo = None
+if os.name == "nt":
+    startupinfo = subprocess.STARTUPINFO()
+
+elections = pd.DataFrame.from_csv("2012-general-election-romney-vs-obama.csv")
+
+elections["Diff"] = elections["Obama"] - elections["Romney"]
+convert = lambda x: dt.datetime.strptime(x, "%Y-%m-%d").date()
+date = elections["Start Date"].apply(convert)
+elections["Start Date"] = date
+# https://stackoverflow.com/questions/9627686/plotting-dates-on-the-x-axis-with-pythons-matplotlib
+five_days = elections[elections["Start Date"] >= dt.datetime.strptime("2012-11-1", "%Y-%m-%d").date()]
+
+# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+# plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+# plt.plot(np.array(five_days["Start Date"]), np.array(five_days["Diff"]), "ro")
+# plt.gcf().autofmt_xdate()
+# plt.show()
+
+elections = elections.reset_index()
+plotter = elections[["Pollster", "Diff"]]
+plotter.boxplot(by="Pollster", rot=45)
+plt.show()
+
